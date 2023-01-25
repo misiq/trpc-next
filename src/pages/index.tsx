108ff62f5bbfefb52f865bb68b3b4627670ab72e
Michal Baran <michal.baran@live.com>
2023-01-25 13:49:00 +0100

    Prisma

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<main className={styles.main}>
-				<h1>Hello</h1>
+				<h1>Hello </h1>
 			</main>
 		</>
 	);
